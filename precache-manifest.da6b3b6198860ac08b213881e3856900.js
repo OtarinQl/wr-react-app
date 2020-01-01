ecd1fd2f394bc337e80f500bdd5d58f1963aecbb
OtarinQl <44878608+OtarinQl@users.noreply.github.com>
2020-01-01 12:42:42 -0300

    Updates

---
+++ precache-manifest.da6b3b6198860ac08b213881e3856900.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d51fa79a2b282051d3985f75da42614",
+    "url": "/weather-report/index.html"
+  },
+  {
+    "revision": "3176db859e679697a6f9",
+    "url": "/weather-report/static/css/main.d5bec0c8.chunk.css"
+  },
+  {
+    "revision": "a1a6201ff3b474ace836",
+    "url": "/weather-report/static/js/2.957ccf45.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/weather-report/static/js/2.957ccf45.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3176db859e679697a6f9",
+    "url": "/weather-report/static/js/main.eb45356a.chunk.js"
+  },
+  {
+    "revision": "060cc0bb67112b3cca7b",
+    "url": "/weather-report/static/js/runtime-main.4487daa6.js"
+  }
+]);
